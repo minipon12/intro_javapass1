68d66154b454d42b29455b95900bc3e3ea312ce9
wacharaporn chaiyakuiwattana <minipon.123456@gmail.com>
2019-08-03 15:26:23 +0700

    SwitchCase JavaBreak

---
+++ SwitchCase.java
@@ -0,0 +1,17 @@
+public class SwitchCase {
+    public static void main(String args[]){
+      int i = 1;
+      switch(i)
+      {
+         case 1:
+       System.out.println("Case1 ");
+       break;
+         case 2:
+       System.out.println("Case2");
+         case 3:
+       System.out.println("Case3");
+         default:
+       System.out.println("Default");
+       }
+    }
+ }
