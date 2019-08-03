64ef7c4aa0a41e186547e6d1e63f2ac5a48503d1
wacharaporn chaiyakuiwattana <minipon.123456@gmail.com>
2019-08-03 14:19:39 +0700

    Variable Java

---
+++ Variable.java
@@ -0,0 +1,17 @@
+public class Variable{
+    public static void main (String[] args){
+        int number = 10;
+        long longNumber = 100;
+        double doubleNumber = 1.5;
+        float floatNumber =3.0f;
+
+        char charText = 'C';
+        String text = "message";
+
+        System.out.println(number);
+        System.out.println(longNumber);
+        System.out.println(doubleNumber);
+        System.out.println(doubleNumber);
+        System.out.println(floatNumber);
+    }
+}
